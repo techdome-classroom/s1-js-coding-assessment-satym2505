46280d12ff0e10904dc95e64c24435311788ffdb
saransh kumar <saranshkumar@Mac.local>
2024-09-30 12:17:20 +0530

    Auto commit on save

---
+++ program1.js
@@ -0,0 +1,21 @@
+let stack = [];
+    let matchingBrackets = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+
+        if (matchingBrackets[char]) {
+            stack.push(char);
+        }
+        else {
+            let topElement = stack.pop();
+            if (matchingBrackets[topElement] !== char) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
